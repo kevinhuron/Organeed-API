9ee4280ab78f9c9bf2beced5db0708662722742a
Kévin <kevin@cosavostra.com>
2016-09-11 18:52:27 +0200

    try fix error server

---
+++ APP/ROUTES/route.js
@@ -0,0 +1,3 @@
+/**
+ * Created by kevinhuron on 11/09/2016.
+ */
--- server.js
+++ server.js
@@ -54,8 +54,9 @@ REST.prototype.configureExpress = function(connection) {
 };
 
 REST.prototype.startServer = function() {
-    app.listen(port, ip);
-    console.log("CONNECTED sur le port " + port);
+    app.listen(port, ip, function(){
+        console.log("CONNECTED sur le port " + port);
+    });
 };
 
 REST.prototype.stop = function(err) {
