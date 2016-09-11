e99863d66167338cf9b1fc850141644ddb4ff175
Kévin <kevin@cosavostra.com>
2016-09-11 19:53:36 +0200

    try add new user

--- APP/ROUTES/route.js
+++ APP/ROUTES/route.js
@@ -9,7 +9,31 @@ function REST_ROUTER(router,connection,bcrypt) {
 REST_ROUTER.prototype.handleRoutes= function(router,connection,bcrypt) {
     router.get("/",function(req,res){
         res.json({"Message" : "YEAH CONNECTED TO THE REST API ROUTER"});
-    })
+    });
+    router.post("/new/user",function(req,res){
+        var query = "INSERT INTO ??(??,??,??) VALUES (?,?,?)";
+        var table = ["USERS","first_name","last_name","email",req.body.f_name,req.body.l_name,req.body.email,bcrypt(req.body.password)];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "User Added !"});
+            }
+        });
+    });
+    router.post("/new/event",function(req,res){
+        var query = "INSERT INTO ??(??,??) VALUES (?,?)";
+        var table = ["EVENTS","user_email","user_password",req.body.email,md5(req.body.password)];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Event Added !"});
+            }
+        });
+    });
 };
 
 module.exports = REST_ROUTER;
