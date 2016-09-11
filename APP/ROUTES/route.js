49d98301c5e1f141ebc4c2a100af1414ddfa399e
Kévin <kevin@cosavostra.com>
2016-09-11 19:35:31 +0200

    route.js

--- APP/ROUTES/route.js
+++ APP/ROUTES/route.js
@@ -1,3 +1,15 @@
 /**
  * Created by kevinhuron on 11/09/2016.
  */
+function REST_ROUTER(router,connection,bcrypt) {
+    var self = this;
+    self.handleRoutes(router,connection,bcrypt);
+}
+
+REST_ROUTER.prototype.handleRoutes= function(router,connection,bcrypt) {
+    router.get("/",function(req,res){
+        res.json({"Message" : "Hello World !"});
+    })
+};
+
+module.exports = REST_ROUTER;
