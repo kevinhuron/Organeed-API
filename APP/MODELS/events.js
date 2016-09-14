8a883e71c24cde02ed68306cd01bb5c8d655fbca
Kévin <kevin@cosavostra.com>
2016-09-14 09:31:25 +0200

    RQ add event

--- APP/MODELS/events.js
+++ APP/MODELS/events.js
@@ -1,24 +1,23 @@
 /**
  * Created by kevinhuron on 11/09/2016.
  */
-var mysql   = require("mysql");
+var mysql = require("mysql");
 
 function Event() {
-    //TODO faire la requete insertion event;
-    /*this.create = function(req, res, connection) {
-        var query = "INSERT INTO ??(??,??,??,??) VALUES (?,?,?,?)";
+    this.create = function(req, res, connection) {
+        var query = "INSERT INTO ??(??,??,??,??,??,??) VALUES (?,?,?,?,?,?)";
         var table = [
-            "USERS","first_name","last_name","email","password",
-            req.query.f_name,req.query.l_name,req.query.email,this.generateHash(req.query.password)
+            "EVENTS","title","date_start","date_end","description","place","id_manager",
+            req.query.title,req.query.date_start,req.query.date_end,req.query.description,req.query.place, // TODO id user
         ];
         query = mysql.format(query,table);
         connection.query(query,function(err,rows){
             if(err) {
                 res.json({"Error" : true, "Message" : "Error executing MySQL query = " + err});
             } else {
-                res.json({"Error" : false, "Message" : "User Added !"});
+                res.json({"Error" : false, "Message" : "Event Added !"});
             }
         });
-    };*/
+    };
 }
 module.exports = new Event();
