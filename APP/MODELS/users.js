bae323d6ec8b385ab399ed7c534a774224607022
Kévin <kevin@cosavostra.com>
2016-09-14 10:02:20 +0200

    test crypt password 2

--- APP/MODELS/users.js
+++ APP/MODELS/users.js
@@ -6,9 +6,28 @@ var bcrypt  = require('bcrypt-nodejs');
 
 function User() {
     /** generate hash password **/
-    this.generateHash = function(password) {
-        return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+    cryptPassword = function(password, callback) {
+        bcrypt.genSalt(10, function(err, salt) {
+            if (err)
+                return callback(err);
+
+            bcrypt.hash(password, salt, function(err, hash) {
+                return callback(err, hash);
+            });
+
+        });
     };
+
+    comparePassword = function(password, userPassword, callback) {
+        bcrypt.compare(password, userPassword, function(err, isPasswordMatch) {
+            if (err)
+                return callback(err);
+            return callback(null, isPasswordMatch);
+        });
+    };
+    /*this.generateHash = function(password) {
+        return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+    };*/
 
     /** checking if password is valid **/
     /*this.validPassword = function(password) {
@@ -20,7 +39,7 @@ function User() {
         var query = "INSERT INTO ??(??,??,??,??) VALUES (?,?,?,?)";
         var table = [
             "USERS","first_name","last_name","email","password",
-            req.query.f_name,req.query.l_name,req.query.email,this.generateHash(req.query.password)
+            req.query.f_name,req.query.l_name,req.query.email,cryptPassword(req.query.password)
         ];
         query = mysql.format(query,table);
         connection.query(query,function(err,rows){
