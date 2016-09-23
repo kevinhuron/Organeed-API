6736d85375443ef7d30ee04a9927bc755f73bb26
Kévin <kevin@cosavostra.com>
2016-09-23 12:14:34 +0200

    passport

--- APP/CONFIG/passport.js
+++ APP/CONFIG/passport.js
@@ -3,8 +3,8 @@ var User = require('../MODELS/users');
 
 module.exports = function (passport) {
 
-    passport.serializeUser(function(User, done) {
-        done(null, User.myusers.id);
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
     });
 
     passport.deserializeUser(function(id, done) {
