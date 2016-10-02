beb35708b084e7354b079c69f2bba49764056f61
Kévin <kevin@cosavostra.com>
2016-10-02 14:44:17 +0200

    try fix add tagger

--- APP/MODELS/assoc_tables/tagger.js
+++ APP/MODELS/assoc_tables/tagger.js
@@ -9,12 +9,14 @@ var mytagger = sequelize.define('LIST', {
     id_tags: {
         type: sequelize.Sequelize.INTEGER,
         allowNull: false,
-        field: 'id_tags'
+        field: 'id_tags',
+        primaryKey: true
     },
     id_comment: {
         type: sequelize.Sequelize.INTEGER,
         allowNull: false,
-        field: 'id_comment'
+        field: 'id_comment',
+        primaryKey: true
     }
 }, {
     freezeTableName: true,
