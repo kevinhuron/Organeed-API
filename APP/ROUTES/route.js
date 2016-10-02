47cbea8d77f1b44a4307e45a1b352581e8a0318b
Kévin <kevin@cosavostra.com>
2016-10-02 21:05:41 +0200

    get comment by tag presque fini

--- APP/ROUTES/route.js
+++ APP/ROUTES/route.js
@@ -315,9 +315,9 @@ module.exports = function(app, passport) {
             "SELECT `COMMENTS`.`id_comment`, `COMMENTS`.`author`, `COMMENTS`.`content`, `COMMENTS`.`date_comment`, " +
             "`COMMENTS`.`img`, `COMMENTS`.`id_event`, `COMMENTS`.`id_comment_1` " +
             "FROM `COMMENTS` INNER JOIN `TAGGER` ON `COMMENTS`.`id_comment` = `TAGGER`.`id_comment` WHERE `TAGGER`.`id_tags` IN(:id_tag)",
-            { replacements: { id_tag: req.query.id_tags }, type: sequelize.QueryTypes.SELECT }
-        ).then(function(comments) {
-            res.status(200).json({"comments":comments});
+            { replacements: { id_tag: [req.query.id_tags] }, type: sequelize.QueryTypes.SELECT }        // TODO: Ids tags par rapport à l'app
+        ).then(function(comments) {                                                                     // TODO: A envoyé en tant que ARRAY (je pense)
+            res.status(200).json({"comments":comments});                                                // TODO: Vérifié la query exécutée
         }).catch(function (e) { /** Erreur dans la récupération des comments by tag **/
             console.log("ERROR : Lors de la récupération des comments by tag");
             res.status(400).json({ message: 'ERROR - Une erreur est survenue !' });
